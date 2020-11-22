f4e9b1652d94a2e05bde33283b42847cf1d39f0a
Sohana Kadiwala <sohakadiwala@gmail.com>
2020-11-22 15:46:47 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      <div className="container">
+      <div className="pxImage1 mid">
+      <div className="container overlay">
+    <div className="text">Design Your own</div>
+  </div>
+
+       <h2>Welcome home</h2>
+      <nav className="nav">
+  <h6>houseandhouse.com</h6>
+        <ul>
+                <li><a href="#">Sample</a></li>
+               <li><a href="#about">Store</a></li>
+                <li><a href="#register">Contact</a></li>
+            </ul>
+        </nav>
+    
+        </div>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+  
+      <nav className="nav">
+        <ul>
+                <li><a href="#">Sample</a></li>
+               <li><a href="#about">Store</a></li>
+                <li><a href="#register">Contact</a></li>
+            </ul>
+        </nav>
+    
+  );
+}
+
+export default Nav;
